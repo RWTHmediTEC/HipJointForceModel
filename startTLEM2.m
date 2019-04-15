16a8a3210294afc266c026865f5242a9ebd354da
MCMF <>
2019-04-15 14:43:37 +0200

    minor edit

--- startTLEM2.m
+++ startTLEM2.m
@@ -1,9 +1,10 @@
 clearvars; close all; opengl hardware
-% [List.f, List.p] = matlab.codetools.requiredFilesAndProducts([mfilename '.m']);
-% List.f = List.f'; List.p = List.p';
 
 addpath(genpath('src'))
 addpath(genpath('data'))
 
 data = createDataTLEM2();
 gui = createInterfaceTLEM2(data);
+
+% [List.f, List.p] = matlab.codetools.requiredFilesAndProducts([mfilename '.m']);
+% List.f = List.f'; List.p = List.p';
