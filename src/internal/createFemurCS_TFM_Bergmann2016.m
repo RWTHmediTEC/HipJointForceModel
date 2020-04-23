038303a049ee38d4312d9ae6ff3c5f3215b53c27
MCMF <>
2020-04-23 08:31:44 +0200

    simplified calculation of bone CS TFM added wrapping test GUI

--- src/internal/createFemurCS_TFM_Bergmann2016.m
+++ src/internal/createFemurCS_TFM_Bergmann2016.m
@@ -1,19 +1,27 @@
 function TFM = createFemurCS_TFM_Bergmann2016(MPC, LPC, P1, P2, HJC, side)
 
-StraightFemurAxis=createLine3d(P2, P1);
+% 2016 - Bergmann et al. - Standardized Loads Acting in Hip Implants:
+%   "The origin of this coordinate system is located at the centre of the 
+%   femoral head. The +z axis points upward and is defined by the line 
+%   connecting the two points where the curved femoral mid-line intersected 
+%   with the neck axis (P1) and where it passes the intercondylar notch 
+%   (P2). The +x axis points laterally and is oriented parallel to the 
+%   proximal contour of the condyles. The +y axis points in the anterior 
+%   direction."
 
-%% inital transformation
+StraightFemurAxis = createLine3d(P2, P1);
+
 % Connection of the most posterior points of the condyles
 PosteriorCondyleAxis = createLine3d(MPC, LPC);
 
 Z = normalizeVector3d(StraightFemurAxis(4:6));
 Y = normalizeVector3d(crossProduct3d(StraightFemurAxis(4:6), PosteriorCondyleAxis(4:6)));
 X = normalizeVector3d(crossProduct3d(Y, Z));
-TFM = inv([[inv([X; Y; Z]), HJC']; [0 0 0 1]]);
 
+TFM = [[X;Y;Z],[0 0 0]'; [0 0 0 1]]*createTranslation3d(-HJC);
 switch side
     case 'L'
-        TFM=createRotationOz(pi)*TFM; %#ok<MINV>
+        TFM=createRotationOz(pi)*TFM;
     case 'R'
     otherwise
         error('Invalid side identifier!')
--- src/internal/createFemurCS_TFM_Wu2002.m
+++ src/internal/createFemurCS_TFM_Wu2002.m
@@ -1,4 +1,18 @@
 function TFM = createFemurCS_TFM_Wu2002(MEC, LEC, HJC,side)
+
+% 2002 - Wu et al. - ISB recommendation on definitions of joint coordinate 
+% systems of various joints for the reporting of human joint motion Part 1
+% Femoral coordinate system—xyz:
+% o: The origin coincident with the right (or left) hip center of rotation, 
+%    coincident with that of the pelvic coordinate system (o) in the 
+%    neutral configuration.
+% y: The line joining the midpoint between the medial and lateral femoral 
+%    epicondyles (FEs) and the origin, and pointing cranially.
+% z: The line perpendicular to the y-axis, lying in the plane defined by 
+%    the origin and the two FEs, pointing to the right.
+% x: The line perpendicular to both y- and z-axis, pointing anteriorly 
+% (Cappozzo et al., 1995)
+
 % Midpoint between the epicondyles
 MEC_LEC_midPoint=midPoint3d(MEC, LEC);
 % Mechanical axis is the connection of EC midpoint and hip joint center
@@ -10,11 +24,10 @@ Y = normalizeVector3d(MechanicalAxis(4:6));
 X = normalizeVector3d(crossProduct3d(MechanicalAxis(4:6), EpicondyleAxis(4:6)));
 Z = normalizeVector3d(crossProduct3d(X, Y));
 
-TFM = inv([[inv([X; Y; Z]), HJC']; [0 0 0 1]]);
-
+TFM = [[X;Y;Z],[0 0 0]'; [0 0 0 1]]*createTranslation3d(-HJC);
 switch side
     case 'L'
-        TFM=createRotationOy(pi)*TFM; %#ok<MINV>
+        TFM=createRotationOy(pi)*TFM;
     case 'R'
     otherwise
         error('Invalid side identifier!')
