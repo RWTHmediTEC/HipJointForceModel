b0bad7849036c63965f272dd550bf0d1424b14c0
MCMF <>
2020-10-26 15:31:43 +0100

    Tolerance added to assertion in convertGlobalHJF2LocalHJF.m

--- src/convertGlobalHJF2LocalHJF.m
+++ src/convertGlobalHJF2LocalHJF.m
@@ -10,7 +10,7 @@ data.HJF.Pelvis.Wu2002.R = transformVector3d(R, data.S.LE(1).positionTFM');
 % Sanity Check
 if data.SurfaceData
     assert(all(ismembertol(data.S.LE(1).positionTFM',...
-        createPelvisCS_TFM_Wu2002_TLEM2(data.S.LE),'ByRows',1)))
+        createPelvisCS_TFM_Wu2002_TLEM2(data.S.LE),1e-6,'ByRows',1)))
 end
 
 % Reverse (=negative) R for femur bone CS
@@ -19,7 +19,7 @@ data.HJF.Femur.Wu2002.R = transformVector3d(-R, data.S.LE(2).positionTFM');
 % Sanity Check
 if data.SurfaceData
     assert(all(ismembertol(data.S.LE(2).positionTFM',...
-        createFemurCS_TFM_Wu2002_TLEM2(data.S.LE, 'R'),'ByRows',1)))
+        createFemurCS_TFM_Wu2002_TLEM2(data.S.LE, 'R'),1e-6,'ByRows',1)))
 end
 % [Bergmann 2016]
 data.HJF.Femur.Bergmann2016.R = transformVector3d(-R, ...
