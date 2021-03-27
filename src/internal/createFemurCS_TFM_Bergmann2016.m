7714b1fd9540efb33736a5697bbfe8f84815de93
MCMF <>
2021-03-27 08:29:20 +0100

    createFemurCS_TFM_Bergmann2016.m: more comments

--- src/internal/createFemurCS_TFM_Bergmann2016.m
+++ src/internal/createFemurCS_TFM_Bergmann2016.m
@@ -1,27 +1,37 @@
 function TFM = createFemurCS_TFM_Bergmann2016(MPC, LPC, P1, P2, HJC, side)
-
+%CREATEFEMURCS_TFM_BERGMANN2016 calculates the transformation into
+% OrthoLoad's femoral bone coordinate system using the 'RAS' orientation 
+% and the hip joint center as origin [Bergmann 2016].
+%
+% Reference:
 % 2016 - Bergmann et al. - Standardized Loads Acting in Hip Implants:
-%   "The origin of this coordinate system is located at the centre of the 
-%   femoral head. The +z axis points upward and is defined by the line 
-%   connecting the two points where the curved femoral mid-line intersected 
-%   with the neck axis (P1) and where it passes the intercondylar notch 
-%   (P2). The +x axis points laterally and is oriented parallel to the 
-%   proximal contour of the condyles. The +y axis points in the anterior 
+%   "The origin of this coordinate system is located at the centre of the
+%   femoral head. The +z axis points upward and is defined by the line
+%   connecting the two points where the curved femoral mid-line intersected
+%   with the neck axis (P1) and where it passes the intercondylar notch
+%   (P2). The +x axis points laterally and is oriented parallel to the
+%   proximal contour of the condyles. The +y axis points in the anterior
 %   direction."
 
+% The straight femur axis is defined by the points P1 and P2 [Bergmann2016]
+% P1 is the point on the neck axis closest to the shaft axis
+% P2 is the intercondylar notch
 StraightFemurAxis = createLine3d(P2, P1);
-
 % Connection of the most posterior points of the condyles
 PosteriorCondyleAxis = createLine3d(MPC, LPC);
-
+% 'RAS' orientation is used by the OrthoLoad femoral coordinate system
+% Z -> [S]uperior
 Z = normalizeVector3d(StraightFemurAxis(4:6));
+% Y -> [A]nterior
 Y = normalizeVector3d(crossProduct3d(StraightFemurAxis(4:6), PosteriorCondyleAxis(4:6)));
+% X -> [R]ight
 X = normalizeVector3d(crossProduct3d(Y, Z));
 
-TFM = [[X;Y;Z],[0 0 0]'; [0 0 0 1]]*createTranslation3d(-HJC);
+% Create transformation
+TFM = [[X;Y;Z],[0 0 0]'; [0 0 0 1]] * createTranslation3d(-HJC);
 switch side
     case 'L'
-        TFM=createRotationOz(pi)*TFM;
+        TFM = createRotationOz(pi)*TFM;
     case 'R'
     otherwise
         error('Invalid side identifier!')
