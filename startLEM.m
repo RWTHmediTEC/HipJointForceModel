a3a30b2f12a56214c7328d1989e00b8772c12338
MCMF <>
2021-04-02 10:00:16 +0200

    moving files

--- startLEM.m
+++ startLEM.m
@@ -1,6 +1,9 @@
 clearvars; close all; opengl hardware
 warning off backtrace; warning off verbose
 
+% Requires the toolbox, add-on or app:
+%   GUI Layout Toolbox by David Sampson
+
 addpath(genpath('src'))
 addpath(genpath('data'))
 
